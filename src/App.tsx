f00bf1e48957fb0f8896d61ed4a6ad65f7be141e
roman <r.levyts@gmail.com>
2024-12-10 19:26:34 -0500

    fix declared but unused errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { Button } from './components/Button';
 import { NavBar } from './components/NavBar';
 import { LeftSideBar } from './components/LeftSideBar';
